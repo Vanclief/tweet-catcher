48625b4b6cb1ca55dd4dd739b253fc94579d88d3
Franco Valencia <franco.avalencia@gmail.com>
2018-02-07 11:59:59 -0600

    Fixed linter errors

--- tweetcatcher/client.py
+++ tweetcatcher/client.py
@@ -9,21 +9,21 @@ class StdOutListener(StreamListener):
     """ A listener handles tweets that are received from the stream.
     This is a basic listener that just prints received tweets to stdout.
     """
+
     def on_data(self, data):
 
         t = json.loads(data)
 
         tweet = {
-                'id': t['id_str'],
-                'username': t['user']['screen_name'],
-                'followers_count': t['user']['followers_count'],
-                'text': t['text'],
-                'hashtags': t['entities']['hashtags'],
-                'created_at': t['created_at'],
-                'language': t['lang']
-                }
+            'id': t['id_str'],
+            'username': t['user']['screen_name'],
+            'followers_count': t['user']['followers_count'],
+            'text': t['text'],
+            'hashtags': t['entities']['hashtags'],
+            'created_at': t['created_at'],
+            'language': t['lang']
+            }
 
-        print(tweet['text'])
         return True
 
     def on_error(self, status):
@@ -33,19 +33,19 @@ class StdOutListener(StreamListener):
 if __name__ == '__main__':
 
     with open("config.yml", 'r') as config_file:
-        config = yaml.load(config_file)
+        CONFIG = yaml.load(config_file)
 
-    consumer_key = config['twitter_api']['consumer_key']
-    consumer_secret = config['twitter_api']['consumer_secret']
-    access_token = config['twitter_api']['access_token']
-    access_token_secret = config['twitter_api']['access_token_secret']
+    CONSUMER_KEY = CONFIG['twitter_api']['consumer_key']
+    CONSUMER_SECRET = CONFIG['twitter_api']['consumer_secret']
+    ACCESS_TOKEN = CONFIG['twitter_api']['access_token']
+    ACCESS_TOKEN_SECRET = CONFIG['twitter_api']['access_token_secret']
 
-    listener = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    LISTERNER = StdOutListener()
+    AUTH = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    AUTH.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-    topics = (config['topics']['bitcoin'])
-    languages = (config['languages'])
+    TOPICS = (CONFIG['topics']['bitcoin'])
+    LANGUAGES = (CONFIG['languages'])
 
-    stream = Stream(auth, listener)
-    stream.filter(track=topics, languages=languages)
+    STREAM = Stream(AUTH, LISTERNER)
+    STREAM.filter(track=TOPICS, languages=LANGUAGES)
